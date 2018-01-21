31ef19823ceacc983ae1de7da89e229efccfcefe
katiek281 <35435602+katiek281@users.noreply.github.com>
2018-01-20 20:15:05 -0500

    Update PaneOrganizer.java

--- PaneOrganizer.java
+++ PaneOrganizer.java
@@ -1,12 +1,10 @@
 package RPS;
 
-import javafx.application.Platform;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
